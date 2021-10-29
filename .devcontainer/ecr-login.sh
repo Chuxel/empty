4318248f88943e1358bd428e738e61707b13596a
Chuck Lantz <clantz@microsoft.com>
2021-10-29 20:24:03 +0000

    verify it works

--- .devcontainer/ecr-login.sh
+++ .devcontainer/ecr-login.sh
@@ -25,5 +25,7 @@ export AWS_ACCESS_KEY_ID="${!access_key_id_env_var}"
 export AWS_SECRET_ACCESS_KEY="${!aws_secret_access_key_env_var}"
 #aws ecr get-login-password --region ${!region_env_var} | docker login --username AWS --password-stdin ${!account_id_env_var}.dkr.ecr.${!region_env_var}.amazonaws.com
 
+touch "$(dirname $0)/ecr-login.sh.done"
+
 # clean up
 #rm -rf "${tmp_root}"
