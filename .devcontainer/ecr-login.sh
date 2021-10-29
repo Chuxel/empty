81ed1601744a53d9a48194b50edb4fa16e21ac3e
Chuck Lantz <chuck_lantz@hotmail.com>
2021-10-29 22:24:50 +0000

    Tweak logging

--- .devcontainer/ecr-login.sh
+++ .devcontainer/ecr-login.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "(*) Starting login at $(date)"
+
 # Get environment variable names with secrets in them
 account_id_env_var=${1:-"ECR_ACCOUNT_ID"}
 region_env_var=${2:-"ECR_REGION"}
@@ -20,10 +22,10 @@ if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then echo "(!) ${secret_access_key_env_var
 
 tmp_root="/tmp/__aws-tmp"
 
-mkdir -p "${tmp_root}"
-
 # Download aws CLI into temp spot if not found
-if ! type aws > /dev/null 2>&1; then
+if ! type aws > /dev/null 2>&1 && [ ! -d "${tmp_root}/aws-cli" ]; then
+    echo "(*) Installing AWS CLI in temp location..."
+    mkdir -p "${tmp_root}"
     curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "${tmp_root}/awscliv2.zip"
     unzip "${tmp_root}/awscliv2.zip" -d "${tmp_root}"
     "${tmp_root}/aws/install" -i ${tmp_root}/aws-cli -b ${tmp_root}/bin
@@ -34,5 +36,5 @@ fi
 aws ecr get-login-password --region ${region} | \
     docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
 
-# clean up
-rm -rf "${tmp_root}"
+echo "(*) Logged in at $(date)"
+
