010d3eed86a53240f1b3240d467961fac2423f96
Chuck Lantz <chuck_lantz@hotmail.com>
2021-10-29 19:54:41 +0000

    Attempt a fix

--- .devcontainer/ecr-login.sh
+++ .devcontainer/ecr-login.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 account_id_env_var=${1:-"ECR_ACCOUNT_ID"}
 region_env_var=${1:-"ECR_REGION"}
 access_key_id_env_var="${2:-"AWS_ACCESS_KEY_ID"}"
@@ -14,10 +12,8 @@ mkdir -p "${tmp_root}"
 
 # Download aws CLI into temp spot if not found
 if ! type aws > /dev/null 2>&1; then
-    mkdir -p /tmp/aws-tmp/bin 
-    curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip -d "${tmp_root}"
-    rm -f awscliv2.zip
+    curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "${tmp_root}/awscliv2.zip"
+    unzip "${tmp_root}/awscliv2.zip" -d "${tmp_root}"
     "${tmp_root}/aws/install" -i ${tmp_root}/aws-cli -b ${tmp_root}/bin
     export PATH="${PATH}:${tmp_root}/bin"
 fi
@@ -37,4 +33,4 @@ aws ecr get-login-password \
     --password-stdin ${!account_id_env_var}.dkr.ecr.${!region_env_var}.amazonaws.com
 
 # clean up
-#rm -rf /tmp/aws-tmp
+#rm -rf "${tmp_root}"
