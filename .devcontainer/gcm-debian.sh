f9b2e934f640606f080fc726eb8634d3147a12bb
Chuck Lantz <chuck_lantz@hotmail.com>
2021-11-01 22:58:50 +0000

    Test

---
+++ .devcontainer/gcm-debian.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+apt-get install -y pass gnupg2
+curl -sSL https://github.com/microsoft/Git-Credential-Manager-Core/releases/download/v2.0.474/gcmcore-linux_amd64.2.0.474.41365.deb -o /tmp/gcm.deb
+dpkg -i /tmp/gcm.deb
+rm -f /tmp/gcm.deb
---
+++ setup-gcm.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+sudo sed -i -E 's/helper =.*//' /etc/gitconfig
+git config --global credential.credentialStore gpg
+git-credential-manager-core configure
+
+if pass ls > /dev/null 2>&1; then
+     exit 0
+fi
+
+# Init "pass"
+cat > /tmp/gpginput <<EOF
+     %echo Generating OpenPGP key for pass
+     Key-Type: default
+     Subkey-Type: default
+     Name-Real: GitHub User ${GITHUB_USER}
+     Name-Comment: Used to store git creds 
+     Name-Email: noreply@github.com
+     Expire-Date: 0
+     %commit
+     %echo done
+EOF
+gpg_output="$(gpg --batch --pinentry-mode loopback --generate-key --passphrase '' /tmp/gpginput)"
+key_id="$(echo "${gpg_output}" | grep -oP 'key\s+\K[^\s]+')"
+pass init ${key_id}
+
+rm -f /tmp/gpginput
