ffef32cb417f9dd71e613e44be4cb5668209dbdf
Chuck Lantz <chuck_lantz@hotmail.com>
2021-11-05 15:07:32 +0000

    Fix

--- .devcontainer/save-config.sh
+++ .devcontainer/save-config.sh
@@ -2,6 +2,9 @@
 set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
+mkdir -p openvpn-tmp
+cd openvpn-tmp
+
 if [ ! -z "${OPENVPN_CONFIG}" ]; then 
     echo "${OPENVPN_CONFIG}" > vpnconfig.ovpn
 fi
--- .devcontainer/start-openvpn.sh
+++ .devcontainer/start-openvpn.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 set -e
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-(nohup openvpn --config vpnconfig.ovpn &) > /tmp/openvpn-launch.log
+mkdir -p openvpn-tmp
+cd openvpn-tmp
+
+# Touch file to make sure this user can read it
+touch openvpn.log
+
+sudo_cmd=""
+if [ "$(id -u)" != "0" ]; then
+    sudo_cmd="sudo"
+fi
+
+nohup ${sudo_cmd} /bin/sh -c "openvpn --config vpnconfig.ovpn --log openvpn.log &" | tee openvpn-launch.log
